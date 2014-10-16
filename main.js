6c45c1facb8ffddafc2e4744ccd02aed2966c7c6
oncletom <hi@oncletom.io>
2014-10-16 12:34:12 +0200

    Interactive update of the UI.

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
 (function(d, iswebview){
-  // display detected user-agent
   var uaElement = d.querySelector('.ua');
-  uaElement.innerHTML = navigator.userAgent;
+  var responses = [].slice.call(d.querySelectorAll('h2[class$="-webview"]'));
 
   // display if we are in a WebView or not
-  var el = d.querySelector(iswebview(navigator.userAgent) ? '.is-webview' : '.is-not-webview');
-  el.removeAttribute('hidden');
+  uaElement.onchange = function(){
+    var result = iswebview(String(this.value).trim());
+    var method;
+
+    responses.forEach(function(el){
+      method = 'setAttribute';
+
+      if (
+        (el.className.contains('is-webview') && result)
+        || (el.className.contains('is-not-webview') && !result)
+      ){
+        method = 'removeAttribute';
+      }
+
+      el[method]('hidden', '');
+    });
+  }
+
+  uaElement.value = navigator.userAgent;
+  uaElement.onchange();
+
 })(window.document, isWebview);
